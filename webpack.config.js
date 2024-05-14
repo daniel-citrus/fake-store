ee53a10c8cb27ef12540a1ced5bce2f596525c3f
Daniel <dandcalvo@gmail.com>
2024-05-13 17:00:04 -0700

    init

--- webpack.config.js
+++ webpack.config.js
@@ -1,24 +1,35 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 const path = require('path');
 
-module.exports = {
-    entry: './src/index.js',
-    module: {
-        rules: [
-            {
-                test: /\.s[ac]ss$/i,
-                use: [
-                    // Creates `style` nodes from JS strings
-                    'style-loader',
-                    // Translates CSS into CommonJS
-                    'css-loader',
-                    // Compiles Sass to CSS
-                    'sass-loader',
-                ],
-            },
+module.exports = (env, argv) => {
+    let production = argv.mode === 'production';
+
+    return {
+        entry: './src/scripts/index.js',
+        module: {
+            rules: [
+                {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                        // Creates `style` nodes from JS strings
+                        'style-loader',
+                        // Translates CSS into CommonJS
+                        'css-loader',
+                        // Compiles Sass to CSS
+                        'sass-loader',
+                    ],
+                },
+            ],
+        },
+        output: {
+            filename: 'main.js',
+            path: path.resolve(__dirname, './dist'),
+        },
+        plugins: [
+            new HtmlWebpackPlugin({
+                title: 'Fake Store',
+                template: './src/template.html',
+            }),
         ],
-    },
-    output: {
-        filename: 'main.js',
-        path: path.resolve(__dirname, 'dist'),
-    },
+    };
 };
